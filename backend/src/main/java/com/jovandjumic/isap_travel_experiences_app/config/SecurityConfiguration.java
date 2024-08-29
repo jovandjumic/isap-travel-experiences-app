2325e0942895270c67e3acbe75eebee7161927af
jovandjumic <jdjumic278@gmail.com>
2024-08-29 04:00:15 +0200

    Implemented addCorsMappings method

--- backend/src/main/java/com/jovandjumic/isap_travel_experiences_app/config/SecurityConfiguration.java
+++ backend/src/main/java/com/jovandjumic/isap_travel_experiences_app/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -22,6 +23,7 @@ import static org.springframework.http.HttpMethod.*;
 public class SecurityConfiguration {
 
     private static final String[] WHITE_LIST_URL = {"/api/auth/**"};
+    private static final String[] ALLOWED_ORIGINS = {"http://localhost:3000"};
 
     @Autowired
     JwtAuthenticationFilter jwtAuthFilter;
@@ -46,6 +48,9 @@ public class SecurityConfiguration {
                 .logout(logout -> logout.logoutUrl("/api/v1/auth/logout").addLogoutHandler(logoutHandler)
                 .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
         );
+
+        http.cors(Customizer.withDefaults());
+
         return http.build();
     }
 
--- backend/src/main/java/com/jovandjumic/isap_travel_experiences_app/controllers/ExperienceController.java
+++ backend/src/main/java/com/jovandjumic/isap_travel_experiences_app/controllers/ExperienceController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/experiences")
 public class ExperienceController {
